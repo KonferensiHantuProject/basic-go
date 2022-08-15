779b18995d567a534de7654f4e3d4d03352b27ac
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2022-08-15 13:16:33 +0700

    Learning Array and Slices

--- main.go
+++ main.go
@@ -3,27 +3,31 @@ package main
 import "fmt"
 
 func main() {
-	name := "Bone"
-	age := 41
 
-	// Print
-	fmt.Print("Halo, \n")
-	fmt.Print("Dunia \n")
-	fmt.Print("Baris Baru")
+	// Membuat array (dalam go array punya length yang fixed)
+	// var ages [3]int = [3]int{20, 25, 30}
+	var agesSecond = [3]int{20, 25, 30}
 
-	// Otomatis line baru (Println)
-	fmt.Println("Halo Bone")
-	fmt.Println("Dadah Bone")
-	fmt.Println("Umurku adalah", age, "dan namaku adalah", name)
+	names := [4]string{"Boni", "Mario", "Bobi", "Bobon"}
+	names[1] = "Hartanto"
 
-	// Foramatting string (Printf) %_ = format specifier
-	fmt.Printf("Umurku adalah %v dan namaku adalah %v \n", age, name)
-	fmt.Printf("Umurku adalah %q dan namaku adalah %q \n", age, name)
-	fmt.Printf("Umurku bertipe %T \n", age)
-	fmt.Printf("Kamu mendapat skor %f poin \n", 55.5)
-	fmt.Printf("Kamu mendapat skor %0.2f poin \n", 55.5) // Membatasi jumlah angka di belakang koma
+	fmt.Println(agesSecond, len(agesSecond))
+	fmt.Println(names, len(names))
 
-	// Sprintf (Menyimpan string yang sudah di format)
-	var str = fmt.Sprintf("Umurku adalah %v dan namaku adalah %v \n", age, name)
-	fmt.Println("String yang di simpan adalah", str)
+	// Slice bisa di manipulasi
+	var scores = []int{31, 45, 150}
+	scores[2] = 12
+
+	// Menggunakan append untuk menambah isi slice
+	scores = append(scores, 66)
+	fmt.Println(scores)
+
+	// Slice ranges (yang kedua tidak akan diambil)
+	rangeOne := names[1:3]
+	rangeTwo := names[2:]
+	rangeThree := names[:3]
+	fmt.Println(rangeOne, rangeTwo, rangeThree)
+
+	rangeOne = append(rangeOne, "Dodi")
+	fmt.Println(rangeOne)
 }
